62a50d844ddd836a277e3793a8ee4fe9d184c25d
Metin Arda Durma <metinardadurma@gmail.com>
2023-09-18 20:34:27 +0300

    a

--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@ class Program
          Console.WriteLine("iyi Geceler");
 
         string sonuc = time<=18 ? "İyi Günler" : "İyi Geceler";
+         
+         sonuc = time>=6 && time<=11 ? "Günaydin" : time<=18 ? "iyi Günler" : "İyi Geceler";
          Console.WriteLine(sonuc);
     }
 }
