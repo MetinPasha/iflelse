30fff9fd1408236cf0a9bbc249b2e3c0236dde52
Metin Arda Durma <metinardadurma@gmail.com>
2023-09-18 20:30:46 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace iflelse;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+          
+        if(time>=6 && time<11)
+         Console.WriteLine("Günaydin");
+        else if(time<=18)
+         Console.WriteLine("iyi Günler");
+        else
+         Console.WriteLine("iyi Geceler");
+
+        string sonuc = time<=18 ? "İyi Günler" : "İyi Geceler";
+         Console.WriteLine(sonuc);
+    }
+}
